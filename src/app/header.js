5af72ec65dc9490a5c6806de6ded29588bdc3ad2
Ben Babington <benb@fsbtech.com>
2019-01-17 16:26:32 +0000

    remove bits

--- src/app/header.js
+++ src/app/header.js
@@ -1,48 +1,52 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const links = [
   {
-    to: '/',
-    text: 'Homepage'
+    to: "/",
+    text: "Homepage"
   },
   {
-    to: '/about',
-    text: 'About'
+    to: "/about",
+    text: "About"
   },
   {
-    to: '/profile/1',
-    text: 'Profile 1'
+    to: "/book-now",
+    text: "Boow Now"
   },
   {
-    to: '/profile/2',
-    text: 'Profile 2'
+    to: "/profile/1",
+    text: "Profile 1"
   },
   {
-    to: '/login',
-    text: 'Login',
+    to: "/profile/2",
+    text: "Profile 2"
+  },
+  {
+    to: "/login",
+    text: "Login",
     auth: false
   },
   {
-    to: '/dashboard',
-    text: 'Dashboard',
+    to: "/dashboard",
+    text: "Dashboard",
     auth: true
   },
   {
-    to: '/logout',
-    text: 'Logout',
+    to: "/logout",
+    text: "Logout",
     auth: true
   },
   {
-    to: '/this-is-broken',
-    text: 'Broken Page'
+    to: "/this-is-broken",
+    text: "Broken Page"
   }
 ];
 
 const isCurrent = (to, current) => {
-  if (to === '/' && current === to) {
+  if (to === "/" && current === to) {
     return true;
-  } else if (to !== '/' && current.includes(to)) {
+  } else if (to !== "/" && current.includes(to)) {
     return true;
   }
 
@@ -50,7 +54,7 @@ const isCurrent = (to, current) => {
 };
 
 const HeaderLink = ({ to, text, current }) => (
-  <li className={isCurrent(to, current) ? 'current' : ''}>
+  <li className={isCurrent(to, current) ? "current" : ""}>
     <Link to={to}>{text}</Link>
   </li>
 );
@@ -62,7 +66,7 @@ export default ({ isAuthenticated, current }) => (
       {links.map((link, index) => {
         const TheLink = <HeaderLink key={index} current={current} {...link} />;
 
-        if (link.hasOwnProperty('auth')) {
+        if (link.hasOwnProperty("auth")) {
           if (link.auth && isAuthenticated) {
             return TheLink;
           } else if (!link.auth && !isAuthenticated) {
