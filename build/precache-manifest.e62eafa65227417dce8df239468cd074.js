e04b4fc25357f53e23b4bdcacd9e312d653dcc48
BenBab <b_babington@windowslive.com>
2019-01-17 22:05:25 +0000

    add build

---
+++ build/precache-manifest.e62eafa65227417dce8df239468cd074.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "3450bb018b2b28a1948a",
+    "url": "/static/js/profile.3450bb01.chunk.js"
+  },
+  {
+    "revision": "20bfbe405dfceaf545ea",
+    "url": "/static/css/main.77c5f5dd.chunk.css"
+  },
+  {
+    "revision": "a603b7943e54386b1a22",
+    "url": "/static/js/homepage.a603b794.chunk.js"
+  },
+  {
+    "revision": "aa1a092164d6da590544",
+    "url": "/static/js/about.aa1a0921.chunk.js"
+  },
+  {
+    "revision": "f0b7b55393b21b911e3f",
+    "url": "/static/js/dashboard.f0b7b553.chunk.js"
+  },
+  {
+    "revision": "d8bfb488ab0ee8b064f0",
+    "url": "/static/js/login.d8bfb488.chunk.js"
+  },
+  {
+    "revision": "db2724fbd37dcf7dab9f",
+    "url": "/static/js/logout.db2724fb.chunk.js"
+  },
+  {
+    "revision": "20bfbe405dfceaf545ea",
+    "url": "/static/js/main.20bfbe40.chunk.js"
+  },
+  {
+    "revision": "3de49a0909e91efd22cf",
+    "url": "/static/js/7.3de49a09.chunk.js"
+  },
+  {
+    "revision": "c7f7c057dd66acc6ec56",
+    "url": "/static/js/8.c7f7c057.chunk.js"
+  },
+  {
+    "revision": "b25240a57830b13b72f6",
+    "url": "/static/js/runtime~main.b25240a5.js"
+  },
+  {
+    "revision": "8caa15b8a896d9bc438e4bf38fae208d",
+    "url": "/static/media/logo.8caa15b8.jpg"
+  },
+  {
+    "revision": "1eab475cc3905841c08c997252f77dde",
+    "url": "/static/media/pekka.1eab475c.jpg"
+  },
+  {
+    "revision": "4d6f8e0da24447e09749bc12d046498c",
+    "url": "/static/media/arvidsson.4d6f8e0d.jpg"
+  },
+  {
+    "revision": "876d88ff975d985520b127fbc37df389",
+    "url": "/index.html"
+  }
+];
