12294cec8485b8e32996fb3cc4388d2bfbd8d2bf
Ben Babington <benb@fsbtech.com>
2019-02-08 19:11:59 +0000

    initial replacement of animations for pose animations

---
+++ build/precache-manifest.b49a6dfe18b5de4f7acd78d28e820872.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "866a0699a7e33140f43a",
+    "url": "/static/js/about.866a0699.chunk.js"
+  },
+  {
+    "revision": "7a7084e0ff52b679e619",
+    "url": "/static/js/0.7a7084e0.chunk.js"
+  },
+  {
+    "revision": "daa21f1715c9e9981fdd",
+    "url": "/static/js/1.daa21f17.chunk.js"
+  },
+  {
+    "revision": "34821cd3dcc7757600e6",
+    "url": "/static/js/2.34821cd3.chunk.js"
+  },
+  {
+    "revision": "8caa15b8a896d9bc438e4bf38fae208d",
+    "url": "/static/media/dino.8caa15b8.jpg"
+  },
+  {
+    "revision": "184741cbe4d4a7d735b4",
+    "url": "/static/js/main.184741cb.chunk.js"
+  },
+  {
+    "revision": "1ef8e6f092e09c7a428f",
+    "url": "/static/js/homepage.1ef8e6f0.chunk.js"
+  },
+  {
+    "revision": "e1f06bcff35ee763ff6e",
+    "url": "/static/js/authAdmin.e1f06bcf.chunk.js"
+  },
+  {
+    "revision": "4d80b33dc3c38adde8fa",
+    "url": "/static/js/admin.4d80b33d.chunk.js"
+  },
+  {
+    "revision": "41b580f5a67a9644510f",
+    "url": "/static/js/runtime~main.41b580f5.js"
+  },
+  {
+    "revision": "5a99a9f3c45293800173",
+    "url": "/static/js/myProfile.5a99a9f3.chunk.js"
+  },
+  {
+    "revision": "8a10690503893f99167f",
+    "url": "/static/js/login.8a106905.chunk.js"
+  },
+  {
+    "revision": "e8da047ec2fc725c69a7",
+    "url": "/static/js/logout.e8da047e.chunk.js"
+  },
+  {
+    "revision": "f4d719e3f2756b80f14c",
+    "url": "/static/js/profile.f4d719e3.chunk.js"
+  },
+  {
+    "revision": "947d3289d67fd988de15",
+    "url": "/static/js/12.947d3289.chunk.js"
+  },
+  {
+    "revision": "7440091e992344ca1029",
+    "url": "/static/js/13.7440091e.chunk.js"
+  },
+  {
+    "revision": "184741cbe4d4a7d735b4",
+    "url": "/static/css/main.756bc1b7.chunk.css"
+  },
+  {
+    "revision": "daa21f1715c9e9981fdd",
+    "url": "/static/css/1.998106f4.chunk.css"
+  },
+  {
+    "revision": "81823efc93cc79a3e8b3ab3f1154cec6",
+    "url": "/index.html"
+  }
+];
